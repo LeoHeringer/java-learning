4eaa4ff7b08a114016a9f404420152ea8509d815
Leonardo Heringer <69641220+LeoHeringer@users.noreply.github.com>
2026-05-03 23:25:27 -0300

    Refactor main method to use 'var' for variable
    Introduced 'var' keyword for variable declaration and added output for it.

---
+++ Var.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int minhaIdade = 20;
+        String meuNome = "Fernanda";
+
+        var nomeVariavel = "Fernanda";
+
+        System.out.println("Nome: " + meuNome);
+        System.out.println("Idade: " + minhaIdade);
+        System.out.println("Nome (var): " + nomeVariavel);
+    }
+}

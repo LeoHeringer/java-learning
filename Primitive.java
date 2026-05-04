742e3eda833e1b198784cfb7c323f653048eb157
Leonardo Heringer <69641220+LeoHeringer@users.noreply.github.com>
2026-05-03 23:30:55 -0300

    Add PrimitiveTypesExample class with basic types

---
+++ Primitive.java
@@ -0,0 +1,26 @@
+public class PrimitiveTypesExample {
+
+    public static void main(String[] args) {
+
+        byte myByte = 100;
+        short myShort = 10000;
+        int myInt = 100000;
+        long myLong = 10000000000L;
+
+        float myFloat = 5.75f;
+        double myDouble = 19.99;
+
+        char myChar = 'F';
+
+        boolean isJavaFun = true;
+
+        System.out.println("Byte: " + myByte);
+        System.out.println("Short: " + myShort);
+        System.out.println("Int: " + myInt);
+        System.out.println("Long: " + myLong);
+        System.out.println("Float: " + myFloat);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Char: " + myChar);
+        System.out.println("Boolean: " + isJavaFun);
+    }
+}

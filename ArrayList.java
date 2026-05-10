1dbd2e8d1a99989e87e0f621dfe7db7682ae0332
Leonardo Heringer <69641220+LeoHeringer@users.noreply.github.com>
2026-05-10 03:47:35 -0300

    Initialize arrays and correct ArrayList operations
    Fix issues with array initialization and ArrayList usage.

---
+++ ArrayList.java
@@ -0,0 +1,37 @@
+Import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte[] myByte = {100};
+        short[] myShort = {10000};
+        int[] myInt = {100000, 200000, 300000};
+        long[] myLong = {10000000000L};
+        float[] myFloat = {5.75f};
+        double[] myDouble = {19.99};
+        char[] myChar = {'F'};
+        boolean[] isJavaFun = {true};
+
+        String[] nameArr = new String[15]
+
+        nomeArr[0] = "Fernan";
+
+        nomeArr[1] = "Fernan";
+
+        ArrayList<String> list = new Arraylist<>();
+        name.add("Fernan");
+      name.add("Leo");
+      name.add("Juan");
+      name.add("Liso");
+
+      //Select
+      System.out.println(list.get(3));
+
+      //Delete
+      list.remove(index:0);
+      list.remove(o:"Leo");
+
+      
+    }
+}
